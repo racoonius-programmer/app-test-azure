027e8280055bbf8a21ac9afd754eea05505b11b0
Franco Reyes <fr.reyesh@duocuc.cl>
2026-09-09 16:25:44 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+    <h1>Ejemplo REACT CON AZURE</h1>
+    <p>Probando config con react msal</p>
+    </main>
+  )
+}
+
+export default App
