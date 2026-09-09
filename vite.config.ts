2cdc7e2a4025c45fcdebefc9a568e562f1ec69b3
Franco Reyes <fr.reyesh@duocuc.cl>
2026-09-09 16:36:23 -0300

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react';
+
+import { fileURLToPath } from 'node:url';
+
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+      main: fileURLToPath(new URL('./index.html', import.meta.url)),
+      redirect: fileURLToPath(
+     new URL('./redirect.html', import.meta.url),
+    ),
+   },
+  },
+ },
+
+});
